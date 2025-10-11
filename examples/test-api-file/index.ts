0f354d6e1e409c4e108eea1d2ff280d384aad13d
blockapicoder <blockapicoder@gmail.com>
2025-10-11 17:32:31 +0200

    dev

--- examples/test-api-file/index.ts
+++ examples/test-api-file/index.ts
@@ -5,93 +5,43 @@ import * as api from "./node_modules/tauri-kargo-tools/dist/api.ts"
 (async () => {
     let rep = await fetch("/api/get-config", { method: "POST" })
     const getConfig = await rep.json()
-    rep = await fetch("/api/explorer", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
+    const client = api.createClient()
 
-        },
-        body: JSON.stringify({
-            path: getConfig.code
-        })
-    })
-    let explorer = await rep.json()
+    let explorer = await client.explorer({ path: getConfig.code })
     test.test("Test explorer ", (t) => {
         t.step("Retourne repertoire avec des éléments", () => {
-            test.assertEquals(explorer.content.length > 0, true)
+            test.assertEquals(explorer.type === "directory" && explorer.content.length > 0, true)
         })
 
     })
-
-    await fetch("/api/current-directory", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-
-        },
-        body: JSON.stringify({
-            path: getConfig.code
-        })
+    await client.setCurrentDirectory({     path: getConfig.code})
+    await client.writeFileText("test/test.txt", 'Text.Test ecriture')
+    explorer = await client.explorer({
+        path: getConfig.code + "\\test"
     })
-    await api.createClient().writeFileText("test/test.txt",'Text.Test ecriture')
-
-    rep = await fetch("/api/explorer", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-
-        },
-        body: JSON.stringify({
-            path: getConfig.code + "\\test"
-        })
-
-    })
-
-    explorer = await rep.json()
 
     test.test("Test ecriture fichier et explorer ", (t) => {
         t.step("Retourne repertoire avec un seul fichier", () => {
-            test.assertEquals(explorer.content.length === 1, true)
-            test.assertEquals(explorer.content[0].name === "test.txt", true)
+            test.assertEquals(explorer.type === "directory" && explorer.content.length === 1, true)
+            test.assertEquals(explorer.type === "directory" && explorer.content[0].type !== "directory" && explorer.content[0].name === "test.txt", true)
         })
 
     })
-    rep = await fetch("/api/file/test/test.txt", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
 
-        }
-    })
-    const content = await rep.text()
+    const content = await client.readFileText("test/test.txt")
     test.test("Test lecture fichier  ", (t) => {
         t.step("contenu du fichier egale à  'Text.Test ecriture'", () => {
             test.assertEquals(content === 'Text.Test ecriture', true)
         })
 
     })
-    await fetch("/api/file/test/test.txt", {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-
-        }
+    await  client.deleteFile("test/test.txt")
+    explorer = await client.explorer({
+        path: getConfig.code + "\\test"
     })
-    rep = await fetch("/api/explorer", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-
-        },
-        body: JSON.stringify({
-            path: getConfig.code + "\\test"
-        })
-
-    })
-    explorer = await rep.json()
     test.test("Test supression fichier et explorer ", (t) => {
         t.step("Retourne repertoire vide ", () => {
-            test.assertEquals(explorer.content.length === 0, true)
+            test.assertEquals(explorer.type === "directory" && explorer.content.length === 0, true)
 
         })
 
