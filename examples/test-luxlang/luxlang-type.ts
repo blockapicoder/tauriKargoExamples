54198fca4fa7e86d0428b69affdbfeda2e13698a
blockapicoder <blockapicoder@gmail.com>
2026-04-19 17:31:13 +0200

    dev

--- examples/test-luxlang/luxlang-type.ts
+++ examples/test-luxlang/luxlang-type.ts
@@ -1,17 +1,17 @@
-import { Arg, Fun, Prog } from "./luxlang-model"
-export interface Contexte {
+import { Arg, Call, Cell, Expr, Fun, PartialCall, Prog } from "./luxlang-model"
+export interface TypeContexte {
     globals: Type[], locals: Type[]
 }
 
 
 export interface TypeFun {
-    type:'fun'
-    code:Fun
+    type: 'fun'
+    code: Fun
 }
 export interface TypePartialCall {
-    type:'partialCall'
-    f: TypeFun | TypePartialCall
-    args:Type []
+    type: 'partialCall'
+    code: Fun
+    args: Type[]
 }
 export interface TypeGen {
     type: 'typeGen'
@@ -19,39 +19,215 @@ export interface TypeGen {
     args: Type[]
 }
 export interface TypeAny {
-    type:'any'
+    type: 'any'
 }
-export interface TypeNull {
-    type:'null'
+export interface TypePrimitif {
+    type: 'null' | "string" | "number" | "boolean"
 }
-
+export interface TypeRef {
+    type: 'ref'
+    name: string
+}
 export interface TypeConst {
     type: 'typeConst'
-    value: number|string|boolean
+    value: number | string | boolean
 }
 export interface TypeUnion {
     type: 'typeUnion'
     args: Type[]
 }
-export  interface TypeMeta {
-    type:'typeMeta'
-    value:TypeBase
+export interface TypeMeta {
+    type: 'typeMeta'
+    value: TypeBase
 }
 
-export type TypeBase =  TypeGen  | TypeConst | TypeUnion | TypePartialCall | TypeFun | TypeAny | TypeNull
+export type TypeBase = TypeGen | TypeRef | TypeConst | TypeUnion | TypePartialCall | TypeFun | TypeAny | TypePrimitif
 export type Type = TypeBase | TypeMeta
 export interface TypeError {
-    type:'error'
+    type: 'error'
 }
 
-abstract class TypeChecker {
-    prog:Prog
-  
-    constructor( prog:Prog) {
+export abstract class TypeChecker {
+    prog: Prog
+
+    constructor(prog: Prog) {
         this.prog = prog
     }
-    computeReturnTypeFun(globals:Type[], f:TypeFun , args:Type []):Type|TypeError {
-        return  { type:'error'}
+    computeType( args: Type[]): Type | TypeError {
+        const globals:Type [] = this.prog.map( (f)=> {
+            return  { type:"fun" , code:f}
+        })
+        return this.computeReturnTypeFun(globals,this.prog[this.prog.length-1],args)
+
     }
-    abstract computeReturnTypeGen( f:TypeGen , args:Type []):Type|TypeError
+    computeReturnTypeFun(globals: Type[], f: Fun, args: Type[]): Type | TypeError {
+        if (f.numArg !== args.length) {
+            return { type: 'error' }
+        }
+        return this.computeReturnTypeFunRec({ globals: globals, locals: [...args] }, f, 0)
+
+    }
+    computeReturnTypeFunRec(context: TypeContexte, f: Fun, i: number): Type | TypeError {
+
+        if (i === f.code.length) {
+            return this.computeTypeForExpr(context, f.ret)
+        }
+        const tmpI = f.code[i]
+        if (tmpI.type === "setGlobal" || tmpI.type === "setLocal") {
+            const tmpType = this.computeTypeForExpr(context, tmpI.value)
+            if (tmpType.type === "error") {
+                return tmpType
+            }
+            if (tmpType.type === "typeUnion") {
+                const rs: TypeUnion = { type: "typeUnion", args: [] }
+                for (const t of tmpType.args) {
+                    const newContext: TypeContexte = { globals: [...context.globals], locals: [...context.locals] }
+                    if (tmpI.type === "setGlobal") {
+                        newContext.globals[tmpI.var] = t
+                    } else {
+                        newContext.locals[tmpI.var] = t
+                    }
+                    const tmpTypeBis = this.computeReturnTypeFunRec(newContext, f, i + 1)
+                    if (tmpTypeBis.type === "error") {
+                        return tmpTypeBis
+                    }
+                    rs.args.push(tmpTypeBis)
+
+                }
+                return rs;
+            }
+            if (tmpI.type === "setGlobal") {
+                context.globals[tmpI.var] = tmpType
+            } else {
+                context.locals[tmpI.var] = tmpType
+            }
+            return this.computeReturnTypeFunRec(context, f, i + 1)
+        }
+        if (tmpI.type === "ifRet") {
+            const tmpType = this.computeTypeForExpr(context, tmpI.if)
+            if (tmpType.type === "error") {
+                return tmpType
+            }
+            if (tmpType.type === "typeConst") {
+                if (tmpType.value !== false && tmpType.value !== 0) {
+                    return this.computeTypeForExpr(context, tmpI.then)
+                }
+            }
+            if (tmpType.type === "boolean" || tmpType.type === "number") {
+                const thenType = this.computeTypeForExpr(context, tmpI.then)
+                if (thenType.type === "error") {
+                    return thenType
+                }
+                const retType = this.computeReturnTypeFunRec(context, f, i + 1)
+                if (retType.type === "error") {
+                    return retType
+                }
+                return { type: "typeUnion", args: [thenType, retType] }
+            }
+            if (tmpType.type === "null") {
+                return this.computeReturnTypeFunRec(context, f, i + 1)
+            }
+        }
+        if (tmpI.type === "call") {
+            this.computeTypeForCall(context, tmpI)
+            return this.computeReturnTypeFunRec(context, f, i + 1)
+        }
+
+
+        return { type: 'error' }
+    }
+    computeTypeForArg(context: TypeContexte, a: Arg): Type | TypeError {
+        if (a.type === "extern") {
+            return { type: "ref", name: a.name }
+        }
+        if (a.type === "global") {
+            return context.globals[a.idx] ?? { type: "error" }
+        }
+        if (a.type === "local") {
+            return context.locals[a.idx] ?? { type: "error" }
+        }
+        if (a.type === "literal")
+            return { type: "typeConst", value: typeof a.value }
+        return { type: "error" }
+    }
+    getTypeForCell(context: TypeContexte, cell: Cell): Type | TypeError {
+        let tmpType = cell.type === "global" ? context.globals[cell.idx] : context.locals[cell.idx]
+
+        if (!tmpType) {
+            return { type: 'error' }
+        }
+        return tmpType
+
+    }
+    computeTypeForCall(context: TypeContexte, expr: Call): Type | TypeError {
+        const args: Type[] = []
+        for (const e of expr.args) {
+            const tmpType = this.computeTypeForArg(context, e)
+            if (tmpType.type === "error") {
+                return tmpType
+            }
+            args.push(tmpType)
+        }
+        if (typeof expr.op === "string") {
+
+            return this.computeReturnRefCall(expr.op, args);
+        }
+        const tmpType = this.getTypeForCell(context, expr.op)
+        if (tmpType.type === "typeGen") {
+            return this.computeReturnTypeGen(tmpType, args)
+        }
+        if (tmpType.type === "fun") {
+            return this.computeReturnTypeFun(context.globals, tmpType.code, args)
+        }
+        if (tmpType.type === "partialCall") {
+            return this.computeReturnTypeFun(context.globals, tmpType.code, [...tmpType.args, ...args])
+        }
+        return { type: 'error' }
+    }
+
+    computeTypeForPartialCall(context: TypeContexte, expr: PartialCall): Type | TypeError {
+        const args: Type[] = []
+        for (const e of expr.args) {
+            const tmpType = this.computeTypeForArg(context, e)
+            if (tmpType.type === "error") {
+                return tmpType
+            }
+            args.push(tmpType)
+        }
+        const tmpType = this.getTypeForCell(context, expr.op)
+        if (tmpType.type === "error") {
+            return tmpType
+        }
+        if (tmpType.type === "partialCall") {
+            return { type: "partialCall", code: tmpType.code, args: [...tmpType.args, ...args] }
+        }
+        if (tmpType.type === "fun") {
+            return { type: "partialCall", code: tmpType.code, args: args }
+        }
+        return { type: 'error' }
+    }
+    computeTypeForExpr(context: TypeContexte, expr: Expr): Type | TypeError {
+        if (expr.type === "extern") {
+            return { type: "ref", name: expr.name }
+        }
+        if (expr.type === "call") {
+            return this.computeTypeForCall(context, expr)
+        }
+        if (expr.type === "partialCall") {
+            return this.computeTypeForPartialCall(context, expr)
+        }
+        if (expr.type === "global") {
+            return this.getTypeForCell(context, expr)
+        }
+        if (expr.type === "local") {
+            return this.getTypeForCell(context, expr)
+        }
+        if (expr.type === "literal") {
+            return { type: 'typeConst', value: expr.value }
+        }
+
+        return { type: 'error' }
+    }
+    abstract computeReturnTypeGen(f: TypeGen, args: Type[]): Type | TypeError
+    abstract computeReturnRefCall(ref: string, args: Type[]): Type | TypeError
 }
